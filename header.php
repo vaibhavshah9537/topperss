a62196d1f55bdd409a8072477dc021972574172d
vaibhavshah9537 <33410633+vaibhavshah9537@users.noreply.github.com>
2017-12-31 14:58:16 +0530

    Initial commit

---
+++ header.php
@@ -0,0 +1,120 @@
+
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Toppers - The Family Salon </title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Free HTML5 Website Template by gettemplates.co" />
+	<meta name="keywords" content="free website templates, free html5, free template, free bootstrap, free website template, html5, css3, mobile first, responsive" />
+	<meta name="author" content="gettemplates.co" />
+    Join our mailing list to stay up to date on our upcoming offers
+  	<!-- Facebook and Twitter integration -->
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+
+	<!-- <link href="https://fonts.googleapis.com/css?family=Droid+Sans" rel="stylesheet"> -->
+
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="css/icomoon.css">
+	<!-- Themify Icons-->
+	<link rel="stylesheet" href="css/themify-icons.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+
+	<!-- Magnific Popup -->
+	<link rel="stylesheet" href="css/magnific-popup.css">
+
+	<!-- Owl Carousel  -->
+	<link rel="stylesheet" href="css/owl.carousel.min.css">
+	<link rel="stylesheet" href="css/owl.theme.default.min.css">
+
+	<!-- Theme style  -->
+	<link rel="stylesheet" href="css/style.css">
+
+	<!-- Modernizr JS -->
+	<script src="js/modernizr-2.6.2.min.js"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+
+	</head>
+<!---<div class="gtco-loader"></div>-->
+
+<div id="page">
+    <nav class="gtco-nav" role="navigation">
+        <div class="gtco-container">
+            <div class="row">
+                <div class="col-md-12 text-right gtco-contact">
+                    <ul class="">
+                        <li><a href="#"><i class="ti-mobile"></i> +1 (0)123 456 7890 </a></li>
+                        <li><a href="http://twitter.com/gettemplatesco"><i class="ti-twitter-alt"></i> </a></li>
+                        <li><a href="#"><i class="icon-mail2"></i></a></li>
+                        <li><a href="#"><i class="ti-facebook"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-4 col-xs-12">
+                    <div id="gtco-logo"><a href="index.php"><img height="200" width="250" src="images/tp1.png"><em></em></a></div>
+                </div>
+                <div class="col-xs-8 text-right menu-1">
+                    <ul>
+                        <li class="active"><a href="index.php">Home</a></li>
+                        <li><a href="about.php">About</a></li>
+                        <li class="has-dropdown">
+                            <a href="services.php">Services</a>
+                            <ul class="dropdown">
+                                <li><a href="#">Web Design</a></li>
+                                <li><a href="#">eCommerce</a></li>
+                                <li><a href="#">Branding</a></li>
+                                <li><a href="#">API</a></li>
+                            </ul>
+                        </li>
+                        <li class="has-dropdown">
+                            <a href="#">Dropdown</a>
+                            <ul class="dropdown">
+                                <li><a href="#">HTML5</a></li>
+                                <li><a href="#">CSS3</a></li>
+                                <li><a href="#">Sass</a></li>
+                                <li><a href="#">jQuery</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="portfolio.php">Portfolio</a></li>
+                        <li><a href="#">Toppers Academy</a></li>
+                        <li><a href="contact.php">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+
+        </div>
+    </nav>
+
+    <header id="gtco-header" class="gtco-cover" role="banner">
+         <div class="overlay"></div>
+         <div class="gtco-container">
+             <div class="row">
+                 <div class="col-md-12 col-md-offset-0 text-left">
+                     <div class="display-t">
+                        <div class="display-tc">
+                             <h1 class="animate-box" data-animate-effect="fadeInUp">Your Business To The Next Level</h1>
+                             <h2 class="animate-box" data-animate-effect="fadeInUp">Free HTML5 Bootstrap Templates Made <em>by</em> <a href="http://gettemplates.co/" target="_blank">GetTemplates.co</a></h2>
+                             <p class="animate-box" data-animate-effect="fadeInUp"><a href="#" class="btn btn-white btn-lg btn-outline">Get In Touch</a></p>
+                         </div>
+                     </div>
+                 </div>
+             </div>
+         </div>
+   </header>
+
+</html>
---
+++ tag.php
@@ -0,0 +1,46 @@
+<?php
+?>
+
+<html>
+<head>
+    <!-- Custom styles for this template -->
+    <link href="css/custom-style.css" rel="stylesheet">
+</head>
+<div class="aboutus-home">
+            <div class="aboutus-home-box">
+                <div class="aboutus-home-box1">
+                    <h1>
+                        How Toppers works
+                    </h1>
+                </div>
+                <div class="aboutus-home-box2">
+                    <img src="images/aboutus/service.png" alt="service" class="img-responsive"/>
+                    <h2>
+                        <span>Select Services</span><br/>
+                        Choose from a wide range of beauty services offered directly to your doorstep.
+                    </h2>
+                </div>
+                <div class="aboutus-home-box3">
+                    <img src="images/aboutus/date.png" alt="date icon image" class="img-responsive"/>
+                    <h2>
+                        <span>Pick Date and Time</span><br/>
+                        Book beauty sessions at your convenience, anytime and anywhere in Bhuj.
+                    </h2>
+                </div>
+                <div class="aboutus-home-box4">
+                    <img src="images/aboutus/receive.png" alt="recieve" class="img-responsive"/>
+                    <h2>
+                        <span>Receive Confirmation</span><br/>
+                        Upon booking, you'll immediately receive confirmation with relevant details.
+                    </h2>
+                </div>
+                <div class="aboutus-home-box5">
+                    <img src="images/aboutus/enjoy.png" alt="pamper yourself" class="img-responsive"/>
+                    <h2>
+                        <span>Enjoy the Experience</span><br/>
+                        Relax and enjoy the pampering because ladies, you deserve it.
+                    </h2>
+                </div>
+            </div>
+</div>
+</html>
